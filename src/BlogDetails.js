949d24038b29ba9b934245ef90d61a3c59273fcd
Thuta <thutaminthway2001@gmail.com>
2022-02-27 23:51:47 +0630

    ending a project

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,19 @@
-import {useParams} from 'react-router-dom';
+import {useParams, useHistory} from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const {id} = useParams();
-    let {data:blog, isPending, isError} = useFetch('http://localhost:8000/blogs/' + id)
+    const {data:blog, isPending, isError} = useFetch('http://localhost:8000/blogs/' + id)
+    const history = useHistory();
+
+    const deleteBlog = () => {
+        fetch('http://localhost:8000/blogs/' + id, {
+            method: "DELETE"
+        }).then(() => {
+            history.push('/')
+        })
+    }
+
     return (
         <div className="blog-details">
             {isPending && <div>Loading...</div>}
@@ -13,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
                     <div>{ blog.body }</div>
+                    <button onClick={deleteBlog}>Delete</button>
                 </article>
             )}
         </div>
